694cc70359d1dbcdd863da78252745e99257b41b
= <=>
2026-01-15 13:13:40 +0900

    1시 wefefwf 병합 전 전송

--- src/main/resources/static/js/boardDetail.js
+++ src/main/resources/static/js/boardDetail.js
@@ -1,8 +1,22 @@
 $(function(){
 	
+	//업데이트 하기 눌렀을 때
+	$(document).on("click","#goUpdateBoard",function(){
+		
+		let pass = $("#pass").val();
+			    if(!pass){
+			        alert("비밀번호를 입력해주세요");
+			        return false;
+			    }
+			    $("#rPass").val(pass);
+			    $("#detailCheckForm").attr("action", "/goUpdateBoard");
+			    $("#detailCheckForm").attr("method", "post");
+			    $("#detailCheckForm").submit();
+	});
+	
+	
 	
 	//삭제 하기 버튼을 눌렀을 때
-	
 	//삭제는 fk반대 순으로
 	$(document).on("click", "#detailDelete", function(){
 	    let pass = $("#pass").val();
